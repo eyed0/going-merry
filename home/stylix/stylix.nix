c0338ef61c7a10887de1c52f4bd43bd0a1591628
heehaw <nldeshmukh000@gmail.com>
2025-07-30 01:03:54 +0530

    stylix theme changed

--- home/stylix/stylix.nix
+++ home/stylix/stylix.nix
@@ -12,22 +12,41 @@
   #stylix.targets.nixos-icons.enable = true;
   #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/vesper.yaml";
   stylix.base16Scheme = {
-    base00 = "000000";
-    base01 = "1E2326"; 
-    base02 = "272E33";
-    base03 = "414B50";
-    base04 = "859289";
-    base05 = "D3C6AA";
-    base06 = "E4E1CD";
-    base07 = "FDF6E3";
-    base08 = "FF8F88";
-    base09 = "F5AD7B";
-    base0A = "FCCE7B";
-    base0B = "A7C08C";
-    base0C = "89BAB4";
-    base0D = "7FBBB3";
-    base0E = "D699B6";
-    base0F = "E67E80";
+
+    # base00 = "000000";
+    # base01 = "1E2326"; 
+    # base02 = "272E33";
+    # base03 = "414B50";
+    # base04 = "859289";
+    # base05 = "D3C6AA";
+    # base06 = "E4E1CD";
+    # base07 = "FDF6E3";
+    # base08 = "FF8F88";
+    # base09 = "F5AD7B";
+    # base0A = "FCCE7B";
+    # base0B = "A7C08C";
+    # base0C = "89BAB4";
+    # base0D = "7FBBB3";
+    # base0E = "D699B6";
+    # base0F = "E67E80";
+
+    base00 = "#1a1d23"; # Default Background - much darker
+    base01 = "#1f2329"; # Lighter Background - darker than original
+    base02 = "#2a2f38"; # Selection Background - significantly darker
+    base03 = "#4a423d"; # Comments, Invisibles - darker brown
+    base04 = "#6b5f5a"; # Dark Foreground - muted brown
+    base05 = "#7a6f65"; # Default Foreground - darker beige
+    base06 = "#9d8a7a"; # Light Foreground - muted tan
+    base07 = "#c7b299"; # Light Background - darker cream
+    base08 = "#5a7a7a"; # Variables - darker teal blue
+    base09 = "#785a2b"; # Integers, Boolean - darker gold
+    base0A = "#785a2b"; # Classes - darker gold (same as base09)
+    base0B = "#3a6a6a"; # Strings - darker teal green
+    base0C = "#5a7a7a"; # Support - darker teal blue (same as base08)
+    base0D = "#5a7a7a"; # Functions - darker teal blue (same as base08)
+    base0E = "#a04545"; # Keywords - darker red
+    base0F = "#7a5219"; # Deprecated - darker brown orange
+
   };
 
   # # fonts settings
--- nixos/stylix.nix
+++ nixos/stylix.nix
@@ -30,45 +30,43 @@
     # base0E = "D699B6";
     # base0F = "E67E80";
 
-    # base00 = "0a0f08";  # Deep forest shadow
-    # base01 = "141a12";  # Dark meadow
-    # base02 = "1f2b1d";  # Shadowed grass
-    # base03 = "2d3c2a";  # Tree bark
-    # base04 = "495a46";  # Weathered stone
-    # base05 = "a8b5a2";  # Soft meadow text
-    # base06 = "c2d0bb";  # Light grass
-    # base07 = "e1f0d9";  # Sunlit highlights
-    # base08 = "8b5a5a";  # Castle brick
-    # base09 = "b8794a";  # Warm earth
-    # base0A = "d4c441";  # Golden meadow
-    # base0B = "5a8b3d";  # Rich pasture green
-    # base0C = "4a7c7c";  # Stream blue
-    # base0D = "5a7ab8";  # Castle tower blue
-    # base0E = "8b6bb8";  # Distant mountain purple
-    # base0F = "8b7355";  # Weathered wood
+    # base00 = "0b0a08";  # Deep shadow - darkest corner
+    # base01 = "1a1612";  # Weathered wood shadow
+    # base02 = "2c241e";  # Stone foundation
+    # base03 = "3e342a";  # Aged timber
+    # base04 = "5a4d3f";  # Moss-covered stone
+    # base05 = "c4b49a";  # Cream cat fur / parchment
+    # base06 = "d9cbb3";  # Light cream
+    # base07 = "f0e6d4";  # Sunlit white fur
+    # base08 = "8b5a3c";  # Rust on old tools
+    # base09 = "b8794a";  # Warm wood grain
+    # base0A = "d4a851";  # Golden sunlight patches
+    # base0B = "6b8b3d";  # Deep garden green
+    # base0C = "4a7c59";  # Shadowed moss
+    # base0D = "5a7a8a";  # Weathered blue-gray
+    # base0E = "7a6b8b";  # Lavender in shadows
+    # base0F = "8b6f47";  # Bark and earth tones
 
-    base00 = "0b0a08";  # Deep shadow - darkest corner
-    base01 = "1a1612";  # Weathered wood shadow
-    base02 = "2c241e";  # Stone foundation
-    base03 = "3e342a";  # Aged timber
-    base04 = "5a4d3f";  # Moss-covered stone
-    base05 = "c4b49a";  # Cream cat fur / parchment
-    base06 = "d9cbb3";  # Light cream
-    base07 = "f0e6d4";  # Sunlit white fur
-    base08 = "8b5a3c";  # Rust on old tools
-    base09 = "b8794a";  # Warm wood grain
-    base0A = "d4a851";  # Golden sunlight patches
-    base0B = "6b8b3d";  # Deep garden green
-    base0C = "4a7c59";  # Shadowed moss
-    base0D = "5a7a8a";  # Weathered blue-gray
-    base0E = "7a6b8b";  # Lavender in shadows
-    base0F = "8b6f47";  # Bark and earth tones
-
-
+    base00 = "#1a1d23"; # Default Background - much darker
+    base01 = "#1f2329"; # Lighter Background - darker than original
+    base02 = "#2a2f38"; # Selection Background - significantly darker
+    base03 = "#4a423d"; # Comments, Invisibles - darker brown
+    base04 = "#6b5f5a"; # Dark Foreground - muted brown
+    base05 = "#7a6f65"; # Default Foreground - darker beige
+    base06 = "#9d8a7a"; # Light Foreground - muted tan
+    base07 = "#c7b299"; # Light Background - darker cream
+    base08 = "#5a7a7a"; # Variables - darker teal blue
+    base09 = "#785a2b"; # Integers, Boolean - darker gold
+    base0A = "#785a2b"; # Classes - darker gold (same as base09)
+    base0B = "#3a6a6a"; # Strings - darker teal green
+    base0C = "#5a7a7a"; # Support - darker teal blue (same as base08)
+    base0D = "#5a7a7a"; # Functions - darker teal blue (same as base08)
+    base0E = "#a04545"; # Keywords - darker red
+    base0F = "#7a5219"; # Deprecated - darker brown orange
     
   };
 
-    # fonts settings
+  # fonts settings
   stylix.fonts = {
     serif = {
       package = pkgs.alegreya;
@@ -81,8 +79,8 @@
     };
 
     monospace = {
-     package = pkgs.iosevka;
-     name = "Iosevka Nerd Font";
+      package = pkgs.iosevka;
+      name = "Iosevka Nerd Font";
     };
 
     emoji = {
